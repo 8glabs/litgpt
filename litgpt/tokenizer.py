c4c967463afd4154afda8a89cc7ebf49895931e6
wen <zwen_020@163.com>
2024-05-23 07:23:52 +0800

    fix

--- litgpt/tokenizer.py
+++ litgpt/tokenizer.py
@@ -102,7 +102,7 @@ class Tokenizer:
             raise NotImplementedError
 
     @property
-    def vocab_size(self) -> int:
+    def vocab_size(self, with_added_tokens=False) -> int:
         if self.backend == "huggingface":
             return self.processor.get_vocab_size(with_added_tokens=False)
         if self.backend == "sentencepiece":
